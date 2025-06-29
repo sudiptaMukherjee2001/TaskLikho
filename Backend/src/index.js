62ea8fee7ae2b4ef34dace0ee6cb1c14c014676d
sudiptaMukherjee2001 <frontenddeveloperabbs@gmail.com>
2025-06-30 00:51:41 +0530

    configure the server

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,3 +1,18 @@
+import app from "./app.js";
 import { databaseConnection } from "./db/index.js";
 
-databaseConnection();
+const prop=databaseConnection();//returns a promise
+console.log("Database connection status:", prop);
+
+prop.then(()=>{
+   const server= app.listen(process.env.PORT || 8001, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8001}`);
+    });
+    
+    server.on("error", (err) => {
+  console.error("Something went wrong on app.listen level", err);
+});
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
