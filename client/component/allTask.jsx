77f82603462d469e8a21e4efe63f30767e7cabad
sudiptaMukherjee2001 <frontenddeveloperabbs@gmail.com>
2025-07-20 01:17:42 +0530

    ✨ feat: clean up AllTask component by removing commented-out code

--- client/component/allTask.jsx
+++ client/component/allTask.jsx
@@ -16,14 +16,6 @@ async function AllTask() {
 
     return (
 
-
-
-        // <CustomBox
-
-        //     as="section"
-        //     marginBottom="2rem"
-        // >
-
         visibleDateCards?.map((visibleTask, visibleIndex) => {
             return (
                 <CustomBox
@@ -63,45 +55,6 @@ async function AllTask() {
             )
         })
     )
-    {/* DUE DATE OVERVIEW BOX START */ }
-    {/* <CustomBox
-                            as="section"
-                            className="day-overView-section"
-                            display="flex"
-                        // border="2px solid red"
-                        >
-                            <DayOverviewBox date={task.dueDate} />
-                        </CustomBox> */}
-    {/* DUE DATE OVERVIEW BOX END */ }
-
-    {/* THIS GRID HOLDS ALL THE TASKS */ }
-    {/* <Grid container spacing={3} */ }
-    {/* > */ }
-    {/* {task?.tasks.map((taskInfo, taskIdx) => ( */ }
-    {/* <Grid item key={taskIdx}
-                                size={{ lg: 4, xl: 3 }}
-                            >
-                                {
-                                    taskInfo.isCompleted === false ?
-                                        <TaskinfoCard
-                                            taskStatusText="inprogress"
-                                            taskCard="inpogress-taskCard"
-                                            taskStatus="task-inprogress"
-                                            taskName={taskInfo.taskName}
-                                            priority={taskInfo.priority}
-                                            taskId={taskInfo._id}
-
-                                        />
-                                        : ""
-                                }
-                            </Grid> */}
-    {/* ))} */ }
-    {/* </Grid> */ }
-
-
-
-
-
 }
 
 export default AllTask;
