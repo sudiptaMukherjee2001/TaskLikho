4190508e7b844bc284a8bc3356834f6360dc6524
sudiptaMukherjee2001 <frontenddeveloperabbs@gmail.com>
2025-07-22 21:14:33 +0530

    ✨ fix: update API URLs to use HTTPS for task operations

--- client/utils/CreateReq.util.js
+++ client/utils/CreateReq.util.js
@@ -4,7 +4,7 @@ export const createTaskReq = async (reqBody) => {
     
     try {
         // const res = await axios.post("http://localhost:8000/api/v1/task/create-task", reqBody)
-        const res = await axios.post("http://task-likho.vercel.app/api/v1/task/create-task", reqBody)
+        const res = await axios.post("https://task-likho.vercel.app/api/v1/task/create-task", reqBody)
         // const res = await axios.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/task/create`, reqBody)
         console.log("this data send to  mongodb ==>", res);
     } catch (error) {
--- client/utils/UpdateTaskStatus.util.js
+++ client/utils/UpdateTaskStatus.util.js
@@ -4,7 +4,7 @@ export const updateTaskStatusReq = async (reqBody) => {
     console.log("Update task status request body:", reqBody);
     
     try{
-        const res= await axios.put("http://task-likho.vercel.app/api/v1/task/update-task-status",reqBody)
+        const res= await axios.put("https://task-likho.vercel.app/api/v1/task/update-task-status",reqBody)
     }catch(err){
         console.log("Update task status api is not working", err.message);
     }
