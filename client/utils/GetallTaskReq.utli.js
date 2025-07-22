f387cd1bbde5af34e83d637b8adc69dbda2a6863
sudiptaMukherjee2001 <frontenddeveloperabbs@gmail.com>
2025-07-22 18:57:08 +0530

    ✨ feat: update API endpoints to use Vercel deployment URLs for task operations

--- client/utils/GetallTaskReq.utli.js
+++ client/utils/GetallTaskReq.utli.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const GetAllTaskReq = async () => {
     try{
-       const res=await axios.get('http://localhost:8000/api/v1/task/')
+       const res=await axios.get('http://task-likho.vercel.app/api/v1/task/')
        
        console.log("Get all task api response", res);
          return res?.data;
