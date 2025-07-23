7afdf717efb28a20982c379688af1dc0f220f09f
sudiptaMukherjee2001 <frontenddeveloperabbs@gmail.com>
2025-07-23 11:55:35 +0530

    ✨ fix: update deleteTaskReq utility to use DELETE method for task deletion

--- client/utils/DeleteReq.util.js
+++ client/utils/DeleteReq.util.js
@@ -4,11 +4,11 @@ export const deleteTaskReq = async (reqBody) => {
     
     try {
         // const res = await axios.post("http://localhost:8000/api/v1/task/create-task", reqBody)
-        const res = await axios.post("https://task-likho.vercel.app/api/v1/task/delete-task", reqBody)
+        const res = await axios.delete("https://task-likho.vercel.app/api/v1/task/delete-task", reqBody)
         // const res = await axios.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/task/create`, reqBody)
         console.log("this data send to  mongodb ==>", res);
     } catch (error) {
-        console.log("create api is not working", error.message);
+        console.log("delete api is not working", error.message);
     }
 
 }
