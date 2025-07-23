2e62dbf07b768e74b66cbebdec44914659174a63
sudiptaMukherjee2001 <frontenddeveloperabbs@gmail.com>
2025-07-23 09:50:41 +0530

    ✨ feat: implement delete task functionality with deleteOneTask controller

---
+++ Backend/src/controller/deleteEachTask.controller.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import { TaskCreation } from "../model/taskCreation.model.js";
+import ApiError from "../utils/apiError.js";
+import ApiResponse from "../utils/apiResponse.js";
+import asyncHandler from "../utils/asyncHandler.js";
+
+const deleteOneTask = asyncHandler(async (req, res) => {
+    const { taskId } = req.body;
+
+    if (mongoose.Types.ObjectId.isValid(taskId)) {
+        throw new ApiError(400, "Invalid Task Id");
+    }
+    const deleteOneTask = await TaskCreation.updateOne(
+        { "tasks._id": taskId },
+        {
+            $pull: {
+                tasks: { _id: taskId }
+            }
+        }
+    )
+    if (!deleteOneTask) {
+        throw new ApiError(404, "Task not found");
+    }
+    return res.status(200).json(
+        new ApiResponse(200, "Task deleted successfully", deleteOneTask)
+    );
+})
+
+export default deleteOneTask;
--- Backend/src/route/task.route.js
+++ Backend/src/route/task.route.js
@@ -4,6 +4,7 @@ const router = Router();
 import createTask from '../controller/createtask.controller.js';
 import getAllTask from '../controller/getAllTask.controller.js';
 import updateTaskStatus from '../controller/updateTaskStatus.controller.js';
+import deleteOneTask from '../controller/deleteEachTask.controller.js';
 
 
 
@@ -13,6 +14,8 @@ router.route('/').get(getAllTask)
 router.route('/create-task').post(createTask);
 // Route to update a task status
 router.route('/update-task-status').put(updateTaskStatus);
+// Delete one task
+router.route('/delete-task').delete(deleteOneTask);
 
 
 
